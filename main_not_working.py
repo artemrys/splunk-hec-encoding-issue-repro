8003c31b69cbe0ac0bc1cc7d588b63f5ff05fa28
Artem Rys <rysartem@gmail.com>
2022-06-14 17:48:33 +0200

    feat: initial repro

---
+++ main_not_working.py
@@ -0,0 +1,17 @@
+import json
+import time
+
+import requests
+
+while True:
+    event = {
+        "index": "main",
+        "event": {"name": "goodbye", "value": "こんにちは世界"},
+    }
+    response = requests.post(
+        "http://127.0.0.1:8088/services/collector/event",
+        headers={"Authorization": "Splunk <your-hec-token>"},
+        data=json.dumps(event),
+    )
+    print(f"Event is sent, status code: {response.status_code}")
+    time.sleep(5)
